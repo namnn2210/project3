2cdd02bea4634c42c869499bdd63849abd0f0321
Ngo Ngoc Nam <namngocngo22@gmail.com>
2019-10-11 00:22:27 +0700

    ADD TABLES

---
+++ src/main/java/ginp14/project3/dao/ProductRepository.java
@@ -0,0 +1,9 @@
+package ginp14.project3.dao;
+
+import ginp14.project3.model.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+}
---
+++ src/main/java/ginp14/project3/model/Team.java
@@ -0,0 +1,38 @@
+package ginp14.project3.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "team")
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "team_id")
+    private int id;
+
+    @Column(name = "team_name")
+    private int name;
+
+    public Team() {
+    }
+
+    public Team(int name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+}
