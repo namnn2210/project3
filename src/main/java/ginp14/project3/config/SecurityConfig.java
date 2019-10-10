2a8ad4a420d82cacbd307953833492c87a0bde61
Ngo Ngoc Nam <namngocngo22@gmail.com>
2019-10-11 00:01:42 +0700

    DONE user authentication

--- src/main/java/ginp14/project3/config/SecurityConfig.java
+++ src/main/java/ginp14/project3/config/SecurityConfig.java
@@ -1,15 +1,38 @@
 package ginp14.project3.config;
 
+import ginp14.project3.service.UserService;
+import ginp14.project3.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserServiceImpl userServiceImp;
+
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userServiceImp).passwordEncoder(bCryptPasswordEncoder());
+    }
 
     @Override
     public void configure(WebSecurity web) throws Exception{
@@ -19,8 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http
-                .authorizeRequests()
+        http.authorizeRequests()
                     .antMatchers("/**").permitAll()
                     .antMatchers("/register").permitAll()
                     .antMatchers("/homepage").permitAll()
@@ -31,6 +53,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .loginPage("/login").permitAll()
                     .loginProcessingUrl("/login")
                     .defaultSuccessUrl("/homepage")
-                    .failureUrl("/login?error=true");
+                    .failureUrl("/login?error=true")
+                    .and()
+                .logout()
+                    .permitAll()
+                    .logoutSuccessUrl("/login");
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+-- Role Table Data
+INSERT INTO role(role_name) VALUES ('USER');
+INSERT INTO role(role_name) VALUES ('MANAGER');
+INSERT INTO role(role_name) VALUES ('ADMIN');
